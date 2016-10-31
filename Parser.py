ffa731febc28f7dae0f77183c74a5b2fe080487f
Geoffrey Ramseyer <ramseyer@uchicago.edu>
2016-10-31 18:27:48 -0500

    Slicing now works for all layers, at least for the cube

--- Parser.py
+++ Parser.py
@@ -1,82 +1,107 @@
 class Point:
-	def __init__(self, x, y, z):
-		self.x = x
-		self.y = y
-		self.z = z
+    def __init__(self, x, y, z):
+        self.x = x
+        self.y = y
+        self.z = z
 
-	def __str__ (self):
-		return "x: "+str(self.x)+" y: "+str(self.y)+" z: "+str(self.z)+"\n"
+    def __str__ (self):
+        return "x: "+str(self.x)+" y: "+str(self.y)+" z: "+str(self.z)+"\n"
 
 class Facet:
-	def __init__(self, normal, v1, v2, v3):
-		self.normal = normal
-		self.v1 = v1
-		self.v2 = v2
-		self.v3 = v3
+    def __init__(self, normal, v1, v2, v3):
+        self.normal = normal
+        self.v1 = v1
+        self.v2 = v2
+        self.v3 = v3
 
-	def isFlat(self, zDiff):
-		# trictly less than, not less than or equal to
-		if ((abs(self.v1.z - self.v2.z) < zDiff) and (abs(self.v3.z - self.v2.z) < zDiff) and (abs(self.v1.z - self.v3.z) < zDiff)):
-			return True
-		return False
+    def isFlat(self, zDiff):
+        # trictly less than, not less than or equal to
+        if ((abs(self.v1.z - self.v2.z) < zDiff) and (abs(self.v3.z - self.v2.z) < zDiff) and (abs(self.v1.z - self.v3.z) < zDiff)):
+            return True
+        return False
 
-	def minus(self, p0, p1):
-		return Point(p0.x - p1.x, p0.y - p1.y, p0.z - p1.z)
+    def minus(self, p0, p1):
+        return Point(p0.x - p1.x, p0.y - p1.y, p0.z - p1.z)
 
-	def calculate(self, planeNormal, planePoint, p0, p1):
-		return (self.dotProduct(planeNormal, (self.minus(planePoint, p0)))/(self.dotProduct(planeNormal, (self.minus(p1, p0)))))
+    def calculate(self, planeNormal, planePoint, p0, p1):
+        return (self.dotProduct(planeNormal, (self.minus(planePoint, p0)))/(self.dotProduct(planeNormal, (self.minus(p1, p0)))))
 
-	def dotProduct(self, first, second):
-		return (first.x * second.x) + (first.y * second.y) + (first.z * second.z)
+    def dotProduct(self, first, second):
+       # print (str(first) + " " + str(second))
+        return (first.x * second.x) + (first.y * second.y) + (first.z * second.z)
 
-	def getIntersectionLine(self, z):
-		planeNormal = Point(0,0,1)
-		planePoint = Point(0,0,z)
-		points = []
-		if ((max(self.v1.z, self.v2.z, self.v3.z) < z) or (min(self.v1.z, self.v2.z, self.v3.z) > z)):
-			return None
-		if (((self.v1.z <= z) and (self.v2.z >= z)) or ((self.v1.z >= z) and (self.v2.z <= z))):
-			result = (self.calculate(planeNormal, planePoint, self.v1, self.v2))
-			points.append ((((abs(self.v1.x - self.v2.x) * result) + min(self.v1.x, self.v2.x)), (abs(self.v1.y - self.v2.y) * result+ min(self.v1.y, self.v2.y))))
-		if (((self.v3.z <= z) and (self.v2.z >= z)) or ((self.v3.z >= z) and (self.v2.z <= z))):
-			result = (self.calculate(planeNormal, planePoint, self.v3, self.v2))
-			points.append ((((abs(self.v3.x - self.v2.x) * result)+ min(self.v3.x, self.v2.x)), (abs(self.v3.y - self.v2.y) * result+ min(self.v3.y, self.v2.y))))
+    def getIntersectionLine(self, z):
+        planeNormal = Point(0,0,1)
+        planePoint = Point(0,0,z)
+        points = []
+        if ((max(self.v1.z, self.v2.z, self.v3.z) < z) or (min(self.v1.z, self.v2.z, self.v3.z) > z)):
+            return None
 
-		if (((self.v1.z <= z) and (self.v3.z >= z)) or ((self.v1.z >= z) and (self.v3.z <= z))):
-			result = (self.calculate(planeNormal, planePoint, self.v1, self.v3))
-			points.append ((((abs(self.v1.x - self.v3.x) * result)+ min(self.v1.x, self.v3.x)), (abs(self.v1.y - self.v3.y) * result+ min(self.v1.y, self.v3.y))))
+        if (((self.v1.z <= z) and (self.v2.z >= z)) or ((self.v1.z >= z) and (self.v2.z <= z))):
+            try:
+                result = (self.calculate(planeNormal, planePoint, self.v1, self.v2))
+                points.append ((((abs(self.v1.x - self.v2.x) * result) + min(self.v1.x, self.v2.x)), (abs(self.v1.y - self.v2.y) * result + min(self.v1.y, self.v2.y))))
+            except ZeroDivisionError:
+                if (self.v1.z==z): 
+                    points.append((self.v1.x, self.v1.y))
+                    points.append((self.v2.x, self.v2.y))
+        if (((self.v3.z <= z) and (self.v2.z >= z)) or ((self.v3.z >= z) and (self.v2.z <= z))):
+            try:
+                result = (self.calculate(planeNormal, planePoint, self.v3, self.v2))
+                points.append ((((abs(self.v3.x - self.v2.x) * result) + min(self.v3.x, self.v2.x)), (abs(self.v3.y - self.v2.y) * result + min(self.v3.y, self.v2.y))))
+            except ZeroDivisionError:
+                if (self.v2.z==z):
+                    points.append((self.v2.x, self.v2.y))
+                    points.append((self.v3.x, self.v3.y))
+        if (((self.v1.z <= z) and (self.v3.z >= z)) or ((self.v1.z >= z) and (self.v3.z <= z))):
+            try:
+                result = (self.calculate(planeNormal, planePoint, self.v1, self.v3))
+                points.append ((((abs(self.v1.x - self.v3.x) * result) + min(self.v1.x, self.v3.x)), (abs(self.v1.y - self.v3.y) * result + min(self.v1.y, self.v3.y))))
+            except ZeroDivisionError:
+                if (self.v3.z==z):
+                    points.append((self.v1.x, self.v1.y))
+                    points.append((self.v3.x, self.v3.y))
+        #if (len(points) > 2):
+        #    return None
+        #if (points[0] == points[1]):
+        #    return None
+        if (len(points) == 4):
+            points = list(set(points))
+        if (len(points)==6):
+            return None
+        if (points[0]==points[1]):
+            return None
+        return points
 
-		return points
-
-	def __str__ (self):
-		return "normal: "+str(self.normal) +" v1: "+str(self.v1)+" v2: "+str(self.v2)+" v3: "+str(self.v3)
+    def __str__ (self):
+        return "normal: "+str(self.normal) +" v1: "+str(self.v1)+" v2: "+str(self.v2)+" v3: "+str(self.v3)
 
 def parse(filename, zDiff):
-	f = open(filename, "r")
-	name = f.readline()
-	next = f.readline()
-	facets = []
-	maxz = -99
-	minz = 99
-	maxy = -99
-	miny = 99
-	maxx = -99
-	minx = 99
-	while (not next[:8] == "endsolid"):
-		normal = next[15:-1].split(" ")
-		f.readline() # outer loop
-		v1 = f.readline()[13:-1].split(" ")
-		v2 = f.readline()[13:-1].split(" ")
-		v3 = f.readline()[13:-1].split(" ")
-		maxz = max(maxz, float(v1[2]), float(v2[2]), float(v3[2]))
-		maxy = max(maxy, float(v1[1]), float(v2[1]), float(v3[1]))
-		maxx = max(maxx, float(v1[0]), float(v2[0]), float(v3[0]))
-		minz = min(minz, float(v1[2]), float(v2[2]), float(v3[2]))
-		miny = min(miny, float(v1[1]), float(v2[1]), float(v3[1]))
-		minx = min(minx, float(v1[0]), float(v2[0]), float(v3[0]))
+    f = open(filename, "r")
+    name = f.readline()
+    next = f.readline()
+    facets = []
+    maxz = -99
+    minz = 99
+    maxy = -99
+    miny = 99
+    maxx = -99
+    minx = 99
+    while (not next[:8] == "endsolid"):
+        normal = next[15:-1].split(" ")
+        f.readline() # outer loop
+        v1 = f.readline()[13:-1].split(" ")
+        v2 = f.readline()[13:-1].split(" ")
+        v3 = f.readline()[13:-1].split(" ")
+        maxz = max(maxz, float(v1[2]), float(v2[2]), float(v3[2]))
+        maxy = max(maxy, float(v1[1]), float(v2[1]), float(v3[1]))
+        maxx = max(maxx, float(v1[0]), float(v2[0]), float(v3[0]))
+        minz = min(minz, float(v1[2]), float(v2[2]), float(v3[2]))
+        miny = min(miny, float(v1[1]), float(v2[1]), float(v3[1]))
+        minx = min(minx, float(v1[0]), float(v2[0]), float(v3[0]))
 
-		facets.append(Facet(Point(float(normal[0]), float(normal[1]), float(normal[2])), Point(float(v1[0]), float(v1[1]), float(v1[2])), Point(float(v2[0]), float(v2[1]), float(v2[2])), Point(float(v3[0]), float(v3[1]), float(v3[2]))))
-		f.readline() # endloop
-		f.readline() # end facet
-		next = f.readline() # facet
-	return facets, minx, maxx, miny, maxy, minz, maxz
+        facets.append(Facet(Point(float(normal[0]), float(normal[1]), float(normal[2])), Point(float(v1[0]), float(v1[1]), float(v1[2])), Point(float(v2[0]), float(v2[1]), float(v2[2])), Point(float(v3[0]), float(v3[1]), float(v3[2]))))
+        f.readline() # endloop
+        f.readline() # end facet
+        next = f.readline() # facet
+    return facets, minx, maxx, miny, maxy, minz, maxz
--- Slicer.py
+++ Slicer.py
@@ -5,81 +5,84 @@
 # calculates extra for flat part
 
 import Parser
+import math
 
 
-def generateSliceData(zdelta):
-    (facets, zmax) = Parser.parse(filename, zdelta)
+def generateSliceData(zdelta, filename):
+    (facets, xmin, xmax, ymin, ymax, zmin, zmax) = Parser.parse(filename, zdelta)
 
     facetdata = {}
-    for z in range(ceil(zmax/zdelta), 0):
-        z = z * zdelta
+    for z in range(int(math.floor(zmin/zdelta)), int(math.ceil(zmax/zdelta))+1):
        # triangles = []
         lines = []
         for facet in facets:
         #    if facet.isExactlyFlat(zdelta):
         #        triangles.add(facet)
        #     else:
-            intersection = facet.getIntersectionLine(z)
+            intersection = facet.getIntersectionLine(z*zdelta)
             if intersection is not None:
-                lines.add(facet.getIntersectionLine(z))
+                lines.append(intersection)
         facetdata[z] = lines
-    return facetdata
+        print "z="+ str(z)
+        print(facetdata[z])
+    return (facetdata, xmin, xmax, ymin, ymax, zmin, zmax)
 
 def intersections(coord, layerdata, flip):
     points = []
-    for ((x1, y1), (x2, y2)) in layerdata:
+    for line in layerdata:
+        first = line[0]
+        second = line[1]
+        x1 = first[0]
+        y1 = first[1]
+        x2 = second[0]
+        y2 = second[1]
         if flip:
-            t1 = x1
+            t = x1
             x1 = y1
-            y1 = t1
-            t2 = x2
+            y1 = t
+            t = x2
             x2 = y2
-            y2 = t2
+            y2 = t
         if (x1<= coord and coord <= x2):
             if x1==x2:
                 continue
-            points.add((((coord-x1)*y1) + ((x2-coord)*y2))/(x1-x2))
+            points.append((((coord-x1)*y1) + ((x2-coord)*y2))/(x2-x1))
         elif (x2<=coord and coord<=x1):
             if x2==x1:
                 continue
-            points.add((((coord-x2)*y2) + ((x1-coord)*y1))/(x2-x1))
-    return makePairs(points.sort())
-
-def findXYMax(facetdata):
-    #Assuming all STL coords are positive, as per STL spec
-    xmax = 0
-    ymax = 0
-    for layerdata in facetdata:
-        for ((x1, y1), (x2, y2)) in layerdata:
-            xmax = max(x1, x2, xmax)
-            ymax = max(y1, y2, ymax)
-    return (xmax, ymax)
-
+            points.append((((coord-x2)*y2) + ((x1-coord)*y1))/(x1-x2))
+    return points
 
 def makePairs(points):
     if points==[]:
         return []
-    return makePairs(points[2:]).addFirst((points[0], points[1]))
+    otherOutput = makePairs(points[2:])
+    otherOutput.insert(0, (points[0], points[1]))
+    return otherOutput
 
 def getXLineSurfaceDiffs(coord, layerdata1, layerdata2, flip):
     #IGNORE TRIANGLES WOOOOOOOOT
-    if (layerdata1 is None):
-        return []
     if (layerdata2 is None):
-        return []
+        print "layerdata2 is None"
+        return makePairs(intersections(coord, layerdata1, flip))
     above = intersections(coord, layerdata1, flip)
     below = intersections(coord, layerdata2, flip)
-    total = intervalSetUnion(above + below)
-    return intervalSetDiff(total, intervalSetIntersect(above, below))
+    print "above="+str(above)
+    print "below="+str(below)
+    totalPoints = above + below
+    totalPoints.sort()
+    total = intervalSetUnion(makePairs(totalPoints))
+    print "total= "+ str(total)
+    return total
 
 def intervalIntersect(interval1, interval2):
-    return (max(interval1.first, interval2.first), min(interval1.second, interval2.second))
+    return (max(interval1[0], interval2[0]), min(interval1[1], interval2[1]))
 
 def intervalUnion(interval1, interval2):
-    return (min(interval1.first, interval2.first), max(interval1.second, interval2.second))
+    return (min(interval1[0], interval2[0]), max(interval1[1], interval2[1]))
 
 def intervalHasOverlap(interval1, interval2):
-    return (interval1.first >= interval2.first and interval1.first <= interval2.second) or (interval1.second >= interval2.first and interval1.second <= interval2.second) or (interval2.first >=interval1.first and interval2.first <= interval1.second)
+    return (interval1[0] >= interval2[0] and interval1[0] <= interval2[1]) or (interval1[1] >= interval2[0] and interval1[1] <= interval2[1]) or (interval2[0] >=interval1[0] and interval2[1] <= interval1[1]) or (interval1[0] >= interval2[0] and interval1[1] <= interval2[1])
 
 def intervalSetUnion(intervals):
     queue = intervals
@@ -91,11 +94,12 @@ def intervalSetUnion(intervals):
         for out in output:
             if intervalHasOverlap(interval, out):
                 output.remove(out)
-                queue.add(intervalUnion(out, interval))
+                queue.append(intervalUnion(out, interval))
                 found = True
                 break
         if (not found):
-            output.add(interval)
+            output.append(interval)
+    return output
 
 #pairwise intersection
 def intervalSetIntersect(intervals1, intervals2):
@@ -103,65 +107,94 @@ def intervalSetIntersect(intervals1, intervals2):
     for int1 in intervals1:
         for int2 in intervals2:
             if intervalHasOverlap(int1, int2):
-                temp.add(intervalIntersect(int1, int2))
+                temp.append(intervalIntersect(int1, int2))
 
     return intervalSetUnion(temp)
 
 def intervalDiff(base, subtracting):
-    if intervalIntersect(base, subtracting):
-        if (subtracting.second >= base.second) and (subtracting.first <= base.first):
+    if intervalHasOverlap(base, subtracting):
+        if (subtracting[1] >= base[1]) and (subtracting[0] <= base[0]):
             return []
-        if (subtracting.second >= base.second) and (subtracting.first > base.first):
-            return [(subtracting.first, base.second)]
-        if (subtracting.second < base.second) and (subtracting.first <= base.first):
-            return [(base.first, subtracting.second)]
-        return [(base.first, subtracting.first), (subtracting.second, base.second)]
-    return base
+        if (subtracting[1] >= base[1]) and (subtracting[0] > base[0]):
+            print "foo"
+            return [(base[0], subtracting[0])]
+        if (subtracting[1] < base[1]) and (subtracting[0] <= base[0]):
+            print "bar"
+            return [(subtracting[1], base[1])]
+        return [(base[0], subtracting[0]), (subtracting[1], base[1])]
+    return [base]
 
 def intervalSetDiff(baseIntervals, subtracting):
-    base = baseIntervals
+    base = list(baseIntervals)
     for sub in subtracting:
+        nextIterIntervals = []
         for b in base:
-            base.remove(b)
-            base.add(intervalDiff(b, sub))
+            diffResult = intervalDiff(b, sub)
+            print(base)
+            print(str(b)+ " - " + str(sub)+" = "+str(diffResult))
+            nextIterIntervals = nextIterIntervals + diffResult
+        base = nextIterIntervals
     return base
 
-def getSupportAndFillIntervals(x, layerData, layerdataBelow, layerdataAbove, accumulatedAbove, flip):
-    diff1 = getXLineSurfaceDiffs(x, layerData, layerDataBelow, flip)
-    diff2 = getXLineSurfaceDiffs(x, layerData, layerDataAbove, flip)
+def getSupportAndFillIntervals(x, xmin, xmax, layerData, layerdataBelow, layerdataAbove, accumulatedAbove, flip):
+    diff1 = getXLineSurfaceDiffs(x, layerData, layerdataBelow, flip)
+    diff2 = getXLineSurfaceDiffs(x, layerData, layerdataAbove, flip)
     surfaceLines = intervalSetUnion(diff1 + diff2)
-    supportAndFill = intervalSetDiff(accumulatedAbove, surfaceLines)
-    support = intervalDiff(accumulatedAbove, supportAndFill[0::2])
+    print ("surfaceLines="+str(surfaceLines))
+    print ("accAbove="+str(accumulatedAbove))
+    maxInterval = [(xmin-1, xmax+1)]
+    supportAndFill = intervalSetDiff(maxInterval, surfaceLines)
+    print ("unsorted="+str(supportAndFill))
+    supportAndFill.sort(cmp=lambda first, second:cmp(first[0], second[0]))
+    print "accAbove="+str(accumulatedAbove)
+    print "suppAndFill="+str(supportAndFill)
+    print "suppAndFill[0::2]="+str(supportAndFill[0::2])
+    support = intervalSetIntersect(accumulatedAbove, supportAndFill[0::2])
     fill = supportAndFill[1::2]
     return (surfaceLines, support, fill, intervalSetUnion(accumulatedAbove + surfaceLines))
 
-def processLayer(z, facetdata, accDataX, accDataY, zdelta, zmax, xdelta, ydelta, supportSpacing, fillSpacing, xmax, ymax):
+def processLayer(z, facetdata, accDataX, accDataY, zdelta, xdelta, ydelta, supportSpacing, fillSpacing, xmin, xmax, ymin, ymax, zmin, zmax):
     layerData = facetdata[z]
-    layerDataAbove = facetdata[z+zdelta]
-    layerDataBelow = facetdata[z-zdelta]
-
-    for x in range(0,ceil(xmax/xdelta)):
-        x = x * xdelta
-        (surfaceLines, support, fill, newAccData) = getSupportAndFillIntervals(x, layerData, layerDataBelow, layerDataAbove, accDataX[x], False)
+    layerDataAbove = None
+    layerDataBelow = None
+    if (z*zdelta+zdelta <=zmax):
+        layerDataAbove = facetdata[z+1]
+    if (z*zdelta-zdelta >=zmin):
+        layerDataBelow = facetdata[z-1]
+    print ("processing layer z=" + str(z))
+    print "layerData = " + str(layerData)
+    print len(layerData)
+    for x in range(int(math.floor(xmin/xdelta)),int(math.ceil(xmax/xdelta))+1):
+        print "x=" + str(x)
+        (surfaceLines, support, fill, newAccData) = getSupportAndFillIntervals(x*xdelta, xmin, xmax, layerData, layerDataBelow, layerDataAbove, accDataX[x], False)
         #TODO output gcode
+        print "done x"
         accDataX[x] = newAccData
+        print ("x=" + str(x) + " surfaceLines=" + str(surfaceLines) + " support = " + str(support) + " fill = " + str(fill))
 
-    for y in range(0,ceil(ymax/ydelta)):
-        y = y * ydelta
-        (surfaceLines, support, fill, newAccData) = getSupportAndFillIntervals(y, layerData, layerDataBelow, layerDataAbove, accDataY[y], True)
-        #TODO output gcode
-        accDataY[y] = newAccData
-
-    
+    #for y in range(int(math.floor(ymin/ydelta)),int(math.ceil(ymax/ydelta))+1):
+    #    y = y * ydelta
+    #    (surfaceLines, support, fill, newAccData) = getSupportAndFillIntervals(y, layerData, layerDataBelow, layerDataAbove, accDataY[y], True)
+    #    #TODO output gcode
+    #    accDataY[y] = newAccData
     return (accDataX, accDataY)
 
-def processAll(xdelta, ydelta, supportSpacing, fillSpacing):
-    facetData = generateFacetData(zdelta)
+def processAll(xdelta, ydelta, zdelta, filename, supportSpacing, fillSpacing):
+    (facetData, xmin, xmax, ymin, ymax, zmin, zmax) = generateSliceData(zdelta, filename)
     accIntervalsX = {}
     accIntervalsY = {}
-    (xmax, ymax) = findXYMax(facetData)
-    for z in range(ceil(zmax/zdelta), 0):
-        z = z * zdelta
-        (accIntervalsX2, accIntervalsY2) = processLayer(z, facetData, accIntervalsX, accIntervalsY, zdelta, zmax, xdelta, ydelta, supportSpacing, fillSpacing, xmax, ymax)
+    for x in range(int(math.floor(xmin/xdelta)),int(math.ceil(xmax/xdelta))+1):
+        accIntervalsX[x] = []
+    for y in range(int(math.floor(ymin/ydelta)),int(math.ceil(ymax/ydelta))+1):
+        accIntervalsY[y] = []
+    print (xmin, xmax, ymin, ymax, zmin, zmax)
+    zrange = (range(int((math.floor(zmin/zdelta))), int(math.ceil(zmax/zdelta))+1))
+    zrange.reverse()
+    print(zrange)
+    for z in zrange:
+        print("z=" + str(z))
+        (accIntervalsX2, accIntervalsY2) = processLayer(z, facetData, accIntervalsX, accIntervalsY, zdelta, xdelta, ydelta, supportSpacing, fillSpacing, xmin, xmax, ymin, ymax, zmin, zmax)
         accIntervalsX = accIntervalsX2
         accIntervalsY = accIntervalsY2
+
+processAll(0.1, 0.1, 0.1, "testData/testcube_20mm.stl", 2, 2)
