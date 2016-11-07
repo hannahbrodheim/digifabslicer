bdeda7ef10b0b9115fb9989d8217d7b02260794e
hannahbrodheim <hannahbrodheim@gmail.com>
2016-11-07 16:37:46 -0600

    updated gcode

--- GCode.py
+++ GCode.py
@@ -16,8 +16,8 @@ G10 - does what M207 says
 import math
 
 class GCodeWriter:
-	starterCode = "M109 S205.000000\nG28 X0 Y0 Z0 \nG92 E0 \nG29\nM207 S0.5"
-	endCode = "M104 S0\nM140 S0\nG91\nG1 E-1 F300\nG28 X0 Y0\nM84\nG90"
+	starterCode = "M109 S205.000000\nG28 X0 Y0 Z0 \nG92 E0 \nG29\nM207 S0.5\n"
+	endCode = "M104 S0\nM140 S0\nG91\nG1 E-1 F300\nG28 X0 Y0\nM84\nG90\n"
 	def __init__(self, filename, zDelta):
 		self.f = open(filename, "w")
 		self.x = 0
@@ -47,18 +47,18 @@ class GCodeWriter:
 			self.y = y1
 		elif (self.distance(self.x, self.y, x1, y1) < self.distance(self.x, self.y, x2, y2)):
 			a = ""#G10\n"
-			a += "M103\n"
+			#a += "M103\n"
 			a += "G1 X"+str(x1)+" Y"+str(y1)+" Z"+str(self.z)+" E0\n"
 			a += "G1 X"+str(x2)+" Y"+str(y2)+" Z"+str(self.z)+" E"+self.calculateE(x1, y1, x2, y2)+"\n"
-			a += "M101\n"
+			#a += "M101\n"
 			self.x = x2
 			self.y = y2
 		else:
 			a = ""#"G10\n"	
-			a += "M103\n"
+			#a += "M103\n"
 			a += "G1 X"+str(x2)+" Y"+str(y2)+" Z"+str(self.z)+" E0\n"
 			a += "G1 X"+str(x1)+" Y"+str(y1)+" Z"+str(self.z)+" E"+self.calculateE(x1, y1, x2, y2)	+"\n"
-			a += "M101\n"
+			#a += "M101\n"
 			self.x = x1
 			self.y = y1
 		self.f.write(a)
@@ -66,9 +66,9 @@ class GCodeWriter:
 	def incrementLayer(self):
 		self.z += self.zDelta
 		a = ""#"G10\n"
-		a += "M103\n"
+		#a += "M103\n"
 		a += "G1 X"+str(self.x) +" Y"+str(self.y) +" Z"+str(self.z)+"\n"
-		a += "M101\n"
+		#a += "M101\n"
 		self.f.write(a)
 
 	def done(self):
