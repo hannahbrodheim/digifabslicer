d47ece42b1cdfd1bc6cf8cdf493e449278eac0ad
hannahbrodheim <hannahbrodheim@gmail.com>
2016-11-07 13:58:47 -0600

    updated gcode commands

--- GCode.py
+++ GCode.py
@@ -1,6 +1,23 @@
+'''
+M101 - extruder on, forward
+M103 - extruder off
+G28 - home the head
+G29- do autoleveling
+G1 arguments 
+- x distance X100
+- y distance Y100
+- z distance Z100
+- extruded quantity E10 (total while moving)
+- feedrate per minute F100
+
+M207 - set amount to retract filament
+G10 - does what M207 says
+'''
 import math
+
 class GCodeWriter:
-	starterCode = "G21\n"
+	starterCode = "G28 X0 Y0 Z0 \nG92 E0 \nG29\nM207 S0.5"
+	endCode = "M104 S0\nM140 S0\nG91\nG1 E-1 F300\nG1 Z+0.5 E-5 X-20 Y-20 F70\nG28 X0 Y0\nM84\nG90"
 	def __init__(self, filename, zDelta):
 		self.f = open(filename, "w")
 		self.x = 0
@@ -29,24 +46,31 @@ class GCodeWriter:
 			self.x = x1
 			self.y = y1
 		elif (self.distance(self.x, self.y, x1, y1) < self.distance(self.x, self.y, x2, y2)):
-			a = "G10 E10\n"
+			a = "G10\n"
 			a += "M103\n"
 			a += "G1 X"+str(x1)+" Y"+str(y1)+" Z"+str(self.z)+" E0\n"
 			a += "G1 X"+str(x2)+" Y"+str(y2)+" Z"+str(self.z)+" E"+self.calculateE(x1, y1, x2, y2)+"\n"
+			a += "M101\n"
 			self.x = x2
 			self.y = y2
 		else:
-			a = "G10 E10\n"	
+			a = "G10\n"	
 			a += "M103\n"
 			a += "G1 X"+str(x2)+" Y"+str(y2)+" Z"+str(self.z)+" E0\n"
 			a += "G1 X"+str(x1)+" Y"+str(y1)+" Z"+str(self.z)+" E"+self.calculateE(x1, y1, x2, y2)	+"\n"
+			a += "M101\n"
 			self.x = x1
 			self.y = y1
 		self.f.write(a)
 
 	def incrementLayer(self):
 		self.z += self.zDelta
-		a = "G10 E10\n"
+		a = "G10\n"
 		a += "M103\n"
 		a += "G1 X"+str(self.x) +" Y"+str(self.y) +" Z"+str(self.z)+"\n"
+		a += "M101\n"
 		self.f.write(a)
+
+	def done(self):
+		self.f.write(self.endCode)
+		self.f.close()
